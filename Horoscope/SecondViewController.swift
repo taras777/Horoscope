76e75551e6a94f13de4dac7510d41cba4bbb8399
Taras <Taras@MacBook-Pro-admin.local>
2016-12-18 02:01:51 +0200

    added files for horoscope

---
+++ Horoscope/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  Horoscope
+//
+//  Created by Taras on 17.12.16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import Foundation
---
+++ Horoscope/ThirdViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ThirdViewController.swift
+//  Horoscope
+//
+//  Created by Taras on 17.12.16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import Foundation
